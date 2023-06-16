3e989900c11f59d885e07a62b72864d89ac33d19
lolichka <123555195+lolik7447@users.noreply.github.com>
2023-06-16 20:54:49 +0700

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,13 +1,24 @@
 #lesson3-6
+import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 
-link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link_pass(browser):
+def test_languages(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    time.sleep(6)
 
+    answer_text_locator = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    answer_text = answer_text_locator.get_attribute('value')
+    x = print(answer_text)
+
+    assert "Ajouter au panier" == answer_text
+
+    print(answer_text)
+
+if __name__ == "__main__":
+    pytest.main()
