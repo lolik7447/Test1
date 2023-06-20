dadd85d712ddd2e6f712edfdf24978311262cbcd
lolichka <123555195+lolik7447@users.noreply.github.com>
2023-06-20 22:48:08 +0700

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_languages(browser):
+def test_find_basket_button(browser):
     browser.get(link)
     time.sleep(6)
 
